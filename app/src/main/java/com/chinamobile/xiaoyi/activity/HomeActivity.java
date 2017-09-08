32a6b1b7fa6b66ad20ca248e9ceefb46108e6acb
malijie <190223629@qq.com>
2017-09-08 10:19:49 +0800

    xiaoyi

--- app/src/main/java/com/chinamobile/xiaoyi/activity/HomeActivity.java
+++ app/src/main/java/com/chinamobile/xiaoyi/activity/HomeActivity.java
@@ -1,31 +1,43 @@
 package com.chinamobile.xiaoyi.activity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.baidu.location.BDLocation;
 import com.baidu.location.BDLocationListener;
 import com.baidu.location.LocationClient;
 import com.baidu.location.LocationClientOption;
 import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MapView;
-import com.baidu.mapapi.map.MarkerOptions;
 import com.baidu.mapapi.map.MyLocationData;
 import com.baidu.mapapi.model.LatLng;
 import com.base.lib.base.AppBaseActivity;
-import com.base.lib.base.BaseActivity;
+import com.base.lib.utils.Logger;
 import com.chinamobile.xiaoyi.R;
+import com.chinamobile.xiaoyi.XiaoYiHelpApplication;
+
 
 /**
  * Created by malijie on 2017/9/7.
  */
 
 public class HomeActivity extends AppBaseActivity {
-    private LocationClient mLocationClient;
+    private static String TAG = HomeActivity.class.getSimpleName();
+    private Button mButtonNaviCenter;
     private MapView mMapView = null;
     private BaiduMap mBaiduMap;
-
+    //定位相关
+    private LocationClient mLocationClient;
+    private MyLocationListener myLocationListener;
+    private LatLng mLatLng = null;
+    private double mLongitude;
+    private double mLatitude;
+    private boolean isFirstIn = true;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -34,49 +46,87 @@ public class HomeActivity extends AppBaseActivity {
         initViews();
         initData();
 
+
+
+
+    }
+
+    private Context getContext(){
+        return XiaoYiHelpApplication.mContext;
     }
 
     @Override
     public void initViews() {
         mMapView = (MapView) findViewById(R.id.bmapView);
+        mButtonNaviCenter = findViewById(R.id.id_home_btn_back_home);
+        mButtonNaviCenter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                centerToMyLocation();
+            }
+        });
+
         mBaiduMap = mMapView.getMap();
-        mBaiduMap.setMapType(BaiduMap.MAP_TYPE_NORMAL);//sadasdsadasd
-
-        mLocationClient = new LocationClient(this);
-        LocationClientOption option = new LocationClientOption();
-
-        //就是这个方法设置为true，才能获取当前的位置信息
-        option.setIsNeedAddress(true);
-        option.setOpenGps(true);
-        option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy );//可选，默认高精度，设置定位模式，高精度，低功耗，仅设备
-        option.setIsNeedAddress(true);
-        option.setCoorType("gcj02");//可选，默认gcj02，设置返回的定位结果坐标系
-        //int span = 1000;
-        //option.setScanSpan(span);//可选，默认0，即仅定位一次，设置发起定位请求的间隔需要大于等于1000ms才是有效的
-        mLocationClient.setLocOption(option);
-
-
+        MapStatusUpdate msu = MapStatusUpdateFactory.zoomTo(15.0f);
+        mBaiduMap.setMapStatus(msu);
+        initLocation();
 
     }
 
-    public class MyLocationListener implements BDLocationListener {
+    private void initLocation() {
+        mLocationClient = new LocationClient(this);
+        myLocationListener = new MyLocationListener();
+        mLocationClient.registerLocationListener(myLocationListener);
+
+        LocationClientOption option = new LocationClientOption();
+        option.setCoorType("bd09ll");
+        option.setIsNeedAddress(true);
+        option.setOpenGps(true);
+        option.setScanSpan(1000);
+
+        mLocationClient.setLocOption(option);
+    }
+
+    private class MyLocationListener implements BDLocationListener{
 
         @Override
         public void onReceiveLocation(BDLocation location) {
-            //Receive Location
-            //经纬度
-            double lati = location.getLatitude();
-            double longa = location.getLongitude();
-            //打印出当前位置
-            Log.i("TAG", "location.getAddrStr()=" + location.getAddrStr());
-            //打印出当前城市
-            Log.i("TAG", "location.getCity()=" + location.getCity());
-            //返回码
-            int i = location.getLocType();
+            MyLocationData data = new MyLocationData.Builder()
+                    .accuracy(location.getRadius())
+                    .latitude(location.getLatitude())
+                    .longitude(location.getLatitude()).build();
+
+            mBaiduMap.setMyLocationData(data);
+
+
+            mLongitude = location.getLongitude();
+            mLatitude = location.getLatitude();
+Logger.mlj("location lat=" + mLatitude + ",log=" + mLongitude);
+
+            if(isFirstIn){
+                centerToMyLocation();
+                isFirstIn = false;
+            }
+
         }
     }
 
+    private void centerToMyLocation(){
+        LatLng latLng = new LatLng(mLatitude,mLongitude);
+        MapStatusUpdate msu = MapStatusUpdateFactory.newLatLng(latLng);
+        mBaiduMap.animateMapStatus(msu);
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        //开启定位
+        mBaiduMap.setMyLocationEnabled(true);
+        if(!mLocationClient.isStarted()){
+            mLocationClient.start();
+        }
+
+    }
 
     @Override
     public void initData() {
@@ -93,9 +143,19 @@ public class HomeActivity extends AppBaseActivity {
         super.onResume();
         mMapView.onResume();
     }
+
+
     @Override
     protected void onPause() {
         super.onPause();
         mMapView.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        //关闭定位
+        mBaiduMap.setMyLocationEnabled(false);
+        mLocationClient.stop();
     }
 }
