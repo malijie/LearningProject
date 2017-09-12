f9d059080bf911b86d4bbe4d8a6ea9eaf0ed3ff5
malijie <190223629@qq.com>
2017-09-12 17:52:52 +0800

    update

---
+++ app/src/main/java/com/chinamobile/xiaoyi/ui/activity/LoginActivity.java
@@ -0,0 +1,25 @@
+package com.chinamobile.xiaoyi.ui.activity;
+
+import com.base.lib.base.AppBaseActivity;
+import com.chinamobile.xiaoyi.R;
+
+/**
+ * Created by malijie on 2017/9/12.
+ */
+
+public class LoginActivity extends AppBaseActivity {
+    @Override
+    protected int getContentViewId() {
+        return R.layout.login_layout;
+    }
+
+    @Override
+    public void initViews() {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+}
