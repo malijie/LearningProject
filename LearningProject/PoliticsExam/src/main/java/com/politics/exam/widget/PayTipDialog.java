4418e6e34f5b69aa3b5628929e76a77febe233dd
malijie <190223629@qq.com>
2018-10-30 00:01:15 +0800

    update video url info

---
+++ LearningProject/PoliticsExam/src/main/java/com/politics/exam/widget/PayTipDialog.java
@@ -0,0 +1,28 @@
+package com.politics.exam.widget;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.view.View;
+
+import com.politics.exam.PoliticsApplication;
+import com.politics.exam.R;
+import com.politics.exam.util.Utils;
+
+/**
+ * Created by malijie on 2018/10/29.
+ */
+
+public class PayTipDialog extends AlertDialog{
+    public PayTipDialog(@NonNull Context context) {
+        this(context,-1);
+        View v = Utils.getView(R.layout.pay_tip_dialog);
+        setView(v);
+
+    }
+
+    public PayTipDialog(@NonNull Context context, int themeResId) {
+        super(context, themeResId);
+    }
+}
